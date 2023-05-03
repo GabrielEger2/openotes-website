0d1909af891525ee3998de9ea59c18c120873e46
Gabriel Eger <52424334+GabrielEger2@users.noreply.github.com>
2023-05-02 22:54:02 -0300

    Navbar animation added
    I've added some basic animations for the  navbar using framer-motion

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className=" bg-white pt-[10000px]">
+        Footer
+    </div>
+  )
+}
+
+export default Footer
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,68 +1,89 @@
 import { useState, useEffect } from 'react';
 import { MdMusicNote, MdClose } from 'react-icons/md';
+import { motion } from 'framer-motion';
 
 const Navbar = () => {
-  const [nav, setNav] = useState(false);  // Setting up a state variable for the navigation menu
+  const [nav, setNav] = useState(false);
+  const [mobileNav, setMobileNav] = useState(false);
+  const [scrollPos, setScrollPos] = useState(0);
 
-  const handleNav = () => {
-    setNav(!nav);  // Toggling the value of the navigation menu state variable
+  const handleMobileNav = () => {
+    setMobileNav(!mobileNav);
   };
 
   useEffect(() => {
-    const handleLinkClick = (event) => {  // Defining a function to handle click events on the navigation menu links
-      event.preventDefault();
-      const target = event.target.hash;
-      const element = document.querySelector(target);
-      if (element) {
-        const topOffset = element.offsetTop;
-        window.scrollTo({  // Scrolling the window to the target element
-          top: topOffset,
-          behavior: 'smooth',
-        });
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+      if (currentScrollPos > scrollPos) {
+        // Scrolling down, make navbar smaller
+        setNav(true);
+      } else {
+        // Scrolling up, make navbar bigger
+        setNav(false);
       }
+      setScrollPos(currentScrollPos);
     };
-    const links = document.querySelectorAll('a[href^="#"]');  // Selecting all the navigation menu links with the href attribute starting with "#"
-    links.forEach((link) => {
-      link.addEventListener('click', handleLinkClick);  // Adding the event listener to each link
-    });
-    return () => {
-      links.forEach((link) => {
-        link.removeEventListener('click', handleLinkClick);  // Removing the event listener from each link on component unmount
-      });
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [scrollPos]);
+
+  useEffect(() => {
+    let timeoutId;
+
+    const handleScrollEnd = () => {
+      timeoutId = setTimeout(() => {
+        setNav(false);
+      }, 500);
     };
+
+    const handleScroll = () => {
+      clearTimeout(timeoutId);
+      handleScrollEnd();
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
   }, []);
-  
+
   return (
-    <div className=" bg-gray-50 fixed top-0 w-full z-50 shadow-lg">
-      <div className="flex justify-between items-center h-20 max-w-[1200px] mx-auto px-6">
-        <h1 className="w-full text-4xl font-bold  text-ongray title-font">
+    <motion.div
+      className={`bg-gray-50 fixed top-0 w-full z-50 shadow-lg ${
+        nav ? 'h-16' : 'h-20' 
+      } transition-all duration-500 ease-in-out`}
+      initial={{ y: -100 }}
+      animate={{ y: 0 }}
+      transition={{ duration: 0.5, ease: 'easeInOut' }}
+    >
+      <div className="flex justify-between items-center h-full max-w-[1200px] mx-auto px-6">
+        <h1 className="w-full text-4xl font-bold mb-1 text-ongray title-font">
           <a href="#home">Openotes</a>
         </h1>
         <ul className=" text-onorange md:flex hidden justify-center">
-          <li className="group px-4 mt-1 text-2xl font-bold transition duration-300">
+          <li className="group px-4 text-2xl font-bold transition duration-300">
             <a href="#home">Home</a>
             <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-onorange"></span>
           </li>
-          <li className=" px-4 mt-1 text-2xl font-bold transition duration-300">
-            <a href="#home">Home</a>
+          <li className="group px-4 text-2xl font-bold transition duration-300">
+            <a href="#home">About</a>
             <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-onorange"></span>
           </li>
-          <li className="group px-4 mt-1 text-2xl font-bold transition duration-300">
-            <a href="#home">Home</a>
+          <li className="group px-4 text-2xl font-bold transition duration-300">
+            <a href="#home">Contact</a>
             <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-onorange"></span>
           </li>
-          <li className="group px-4 mt-1 text-2xl font-bold transition duration-300">
-            <a href="#home">Home</a>
+          <li className="group px-4 text-2xl font-bold transition duration-300">
+            <a href="#home">Support</a>
             <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-onorange"></span>
           </li>
         </ul>
-        <div className="text-onorange block md:hidden" onClick={handleNav}>
-          {!nav ? <MdMusicNote size={30} /> : <MdClose size={36} />}
+        <div className="text-onwhite bg-onorange p-1 rounded-full block md:hidden" onClick={handleMobileNav}>
+          {!mobileNav ? <MdMusicNote size={30} /> : <MdClose size={30} />}
         </div>
         <div className=" z-50">
           <div
             className={
-              !nav
+              !mobileNav
                 ? 'ease-in-out duration-100 fixed left-[-100%]'
                 : 'fixed left-0 top-0 w-[60%] h-full border-r border-r-ongray bg-gray-50 ease-in-out duration-500'
             }
@@ -75,17 +96,20 @@ const Navbar = () => {
                 <a href="#home">Home</a>
               </li>
               <li className="p-4 text-2xl border-b border-gray-100">
-                <a href="#menu">Menu</a>
+                <a href="#home">About</a>
               </li>
               <li className="p-4 text-2xl border-b border-gray-100">
-                <a href="#contact">Contact</a>
+                <a href="#home">Contact</a>
+              </li>
+              <li className="p-4 text-2xl border-b border-gray-100">
+                <a href="#home">Support</a>
               </li>
             </ul>
           </div>
         </div>
       </div>
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
-export default Navbar
+export default Navbar;
